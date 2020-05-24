c4c56ee3c03b316de74bb9380120405f2b779251
Chris Tanner <me@chris.dev>
2020-05-24 02:46:44 -0700

    stenrsti

--- ingest/wa/bl/message-handler.js
+++ ingest/wa/bl/message-handler.js
@@ -2,15 +2,15 @@
 const influx = require('../db/influx.js');
 const redis = require('../db/redis')
 
-const _checkExisting = async (client, msg) => {
+const _checkExisting = async (msg) => {
   
-  const unknownIds = await redis.getUnknownContacts([ msg.id.id]);
+  const unknownIds = await redis.getUnknownContacts([ msg.id.user]);
   const newContacts = await utils.getUserInfo(unknownIds);
   
   console.log('savign new contacts: ', newContacts)
   await redis.saveContacts(newContacts);
 }
-const handleMessage = async (client, msg) => {
+const handleMessage = async (msg) => {
   console.log(msg);
   await _checkExisting(client, msg);
   await influx.writeEvents([msg]);
